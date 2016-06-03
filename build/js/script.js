ff8711e05c4dd03dd32879bba0e5982fd2450523
Alecsandr <alecsandrcherneckiy@gmail.com>
2016-06-03 17:13:12 +0300

    first

---
+++ build/js/script.js
@@ -0,0 +1,19 @@
+//Так приятней :)
+window.log = function(param){
+    console.log(param);
+};
+$(document).ready(function(){
+
+    //Chrome Smooth Scroll
+    try {
+        $.browserSelector();
+        if($("html").hasClass("chrome")) {
+            $.smoothScroll();
+        }
+    } catch(err) {
+
+    };
+
+    //alert('ok');
+
+});
